6cb971be6ebce15f3d7f42d3ec7668158a5e3246
Campbell <960148@cognizant.com>
2023-08-22 12:03:54 -0500

    Added boiler plate for task form

---
+++ src/component/TaskForm/task.jsx
@@ -0,0 +1,13 @@
+const TaskFormComponent = () => {
+    return(
+        <>
+            <form>
+                <input id='taskName' type="text"/>
+                <input id='description' type="text"/>
+                <input id='date' type="text"/>
+            </form>
+        </>
+    )
+}
+
+export default TaskFormComponent
